d0d08533dddb02a9a361df0d6e62e520ed443589
Zizazr <filovtv999@gmail.com>
2025-03-18 23:20:40 +0300

    Now all contents in json. Added tools sector

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_moment import Moment
+
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
-
+moment = Moment(app)
+moment.init_app(app)
 from src import routes
--- src/routes.py
+++ src/routes.py
@@ -1,7 +1,10 @@
 from src import app
 from flask import render_template
+import json
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    with open("data/content.json") as f:
+        content = json.load(f)
+    return render_template('index.html', content=content)
