7f2697d71adebd4e038a535638ee29381702818f
Zizazr <filovtv999@gmail.com>
2025-03-19 23:20:15 +0300

    Added switching between languages and some links

--- src/routes.py
+++ src/routes.py
@@ -1,7 +1,8 @@
 from src import app
-from flask import render_template, make_response, request
+from flask import render_template, make_response, request, jsonify
 import json
-from src.utils import get_locale, deep_update
+from src.utils import get_locale, update_json, get_next_lang
+
 
 @app.route('/')
 @app.route('/index')
@@ -10,19 +11,22 @@ def index():
         content = json.load(f)
 
     page_lang = get_locale()
+
+    next_lang= get_next_lang()
+
     try:
         with open(f"data/content.{page_lang}.json", encoding='utf-8') as f:
             content_loc = json.load(f)
-        content = deep_update(content, content_loc)
+        content = update_json(content, content_loc)
     except FileNotFoundError:
         print(f"[ERROR] content file for language {page_lang} not found.")
     
     
-    return render_template('index.html', content=content, page_lang=page_lang)
+    return render_template('index.html', content=content, page_lang=page_lang, next_lang=next_lang)
 
 @app.route('/change_language')
 def change_language():
     lang = request.args.get('l')
-    res = make_response()
+    res = make_response(jsonify({"new": lang, "last": get_locale()}))
     res.set_cookie('lang', lang)
     return res
--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,41 @@
 from flask import request
 from src import app
-import collections
 
 def get_locale():
-    print(request.accept_languages)
     user_lang = request.cookies.get('lang')
     if user_lang in app.config['LANGUAGES']:
         return user_lang
     else:
         return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-def deep_update(d, u):
-    for k, v in u.items():
-        if isinstance(v, collections.abc.Mapping):
-            d[k] = deep_update(d.get(k, {}), v)
-        else:
-            d[k] = v
-    return d
+def get_next_lang():
+    languages = app.config['LANGUAGES']
+    next_index = languages.index(get_locale()) + 1
+    print(next_index)
+    if next_index < len(languages):
+        return languages[next_index]
+    else:
+        return languages[0]
+    
+
+
+def update_json(original, update):
+    if isinstance(original, dict) and isinstance(update, dict):
+        for key, value in update.items():
+            if key in original:
+                if isinstance(value, (dict, list)) and isinstance(original[key], type(value)):
+                    update_json(original[key], value)
+                else:
+                    original[key] = value
+            else:
+                original[key] = value
+    elif isinstance(original, list) and isinstance(update, list):
+        for i, item in enumerate(update):
+            if i < len(original):
+                if isinstance(item, (dict, list)) and isinstance(original[i], type(item)):
+                    update_json(original[i], item)
+                else:
+                    original[i] = item
+            else:
+                original.append(item)
+    return original
